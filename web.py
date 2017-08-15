2ccf8abfb84fb12d0e7d9075a4efb3ada57a3444
liukuan <123@321>
2017-08-15 17:39:10 +0800

    test

--- web.py
+++ web.py
@@ -9,7 +9,7 @@ app = bottle.Bottle()
 
 @app.route('/hello')
 def hello():
-    return 'helloworld1234'
+    return 'helloworld'
 
 if __name__ == '__main__':
     app.run(host = DEFAULT_HOST, port = DEFAULT_PORT)
